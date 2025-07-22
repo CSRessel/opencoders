7b0c96230cfd02da9a807e476c3e74940f17b18f
Clifford Ressel <CSRessel@gmail.com>
2025-07-22 14:56:16 -0400

    feat: Support mouse scroll in fullscreen

--- src/app/event_subscriptions.rs
+++ src/app/event_subscriptions.rs
@@ -2,7 +2,7 @@ use crate::app::{
     event_msg::{Msg, Sub},
     tea_model::{AppState, Model},
 };
-use crossterm::event::{self, Event, KeyCode, KeyModifiers};
+use crossterm::event::{self, Event, KeyCode, KeyModifiers, MouseEventKind};
 
 pub fn subscriptions(model: &Model) -> Vec<Sub> {
     match model.state {
@@ -16,36 +16,45 @@ pub fn poll_subscriptions(model: &Model) -> Result<Option<Msg>, Box<dyn std::err
 
     if subs.contains(&Sub::KeyboardInput) {
         if event::poll(std::time::Duration::from_millis(16))? {
-            if let Event::Key(key) = event::read()? {
-                return Ok(crossterm_to_msg(key, model));
-            }
+            return Ok(crossterm_to_msg(event::read()?, &model));
         }
     }
 
     Ok(None)
 }
 
-fn crossterm_to_msg(key: crossterm::event::KeyEvent, model: &Model) -> Option<Msg> {
-    match (&model.state, key.code, key.modifiers) {
-        // Global quit commands
-        (_, KeyCode::Char('q'), KeyModifiers::CONTROL) => Some(Msg::Quit),
-        (AppState::Welcome, KeyCode::Char('q'), _) => Some(Msg::Quit),
-        (AppState::Welcome, KeyCode::Esc, _) => Some(Msg::Quit),
+fn crossterm_to_msg(event: Event, model: &Model) -> Option<Msg> {
+    match event {
+        Event::Key(key) => {
+            match (&model.state, key.code, key.modifiers) {
+                // Global quit commands
+                (_, KeyCode::Char('q'), KeyModifiers::CONTROL) => Some(Msg::Quit),
+                (AppState::Welcome, KeyCode::Char('q'), _) => Some(Msg::Quit),
+                (AppState::Welcome, KeyCode::Esc, _) => Some(Msg::Quit),
 
-        // State transitions
-        (AppState::Welcome, KeyCode::Enter, _) => Some(Msg::ChangeState(AppState::TextEntry)),
-        (AppState::TextEntry, KeyCode::Esc, _) => Some(Msg::ChangeState(AppState::Welcome)),
+                // State transitions
+                (AppState::Welcome, KeyCode::Enter, _) => {
+                    Some(Msg::ChangeState(AppState::TextEntry))
+                }
+                (AppState::TextEntry, KeyCode::Esc, _) => Some(Msg::ChangeState(AppState::Welcome)),
 
-        // Text input events
-        (AppState::TextEntry, KeyCode::Char(c), _) => Some(Msg::KeyPressed(c)),
-        (AppState::TextEntry, KeyCode::Backspace, _) => Some(Msg::Backspace),
-        (AppState::TextEntry, KeyCode::Enter, _) => Some(Msg::SubmitInput),
+                // Text input events
+                (AppState::TextEntry, KeyCode::Char(c), _) => Some(Msg::KeyPressed(c)),
+                (AppState::TextEntry, KeyCode::Backspace, _) => Some(Msg::Backspace),
+                (AppState::TextEntry, KeyCode::Enter, _) => Some(Msg::SubmitInput),
 
-        // Message log scrolling
-        (AppState::TextEntry, KeyCode::PageUp, _) => Some(Msg::ScrollMessageLog(-1)),
-        (AppState::TextEntry, KeyCode::PageDown, _) => Some(Msg::ScrollMessageLog(1)),
+                // Message log scrolling
+                (AppState::TextEntry, KeyCode::PageUp, _) => Some(Msg::ScrollMessageLog(-1)),
+                (AppState::TextEntry, KeyCode::PageDown, _) => Some(Msg::ScrollMessageLog(1)),
 
+                _ => None,
+            }
+        }
+        Event::Mouse(mouse) => match (&model.state, mouse.kind) {
+            (AppState::TextEntry, MouseEventKind::ScrollUp) => Some(Msg::ScrollMessageLog(-1)),
+            (AppState::TextEntry, MouseEventKind::ScrollDown) => Some(Msg::ScrollMessageLog(1)),
+            _ => None,
+        },
         _ => None,
     }
 }
-
--- src/app/ui_components/terminal.rs
+++ src/app/ui_components/terminal.rs
@@ -17,7 +17,7 @@ impl TerminalGuard {
     ) -> Result<(Self, Terminal<CrosstermBackend<io::Stdout>>), Box<dyn std::error::Error>> {
         enable_raw_mode()?;
         let mut stdout = io::stdout();
-        // execute!(stdout, EnableMouseCapture)?;
+        execute!(stdout, EnableMouseCapture)?;
 
         if !model.inline_mode {
             execute!(stdout, EnterAlternateScreen)?;
@@ -49,7 +49,7 @@ impl Drop for TerminalGuard {
     fn drop(&mut self) {
         let _ = disable_raw_mode();
         let mut stdout = io::stdout();
-        // let _ = execute!(stdout, DisableMouseCapture);
+        let _ = execute!(stdout, DisableMouseCapture);
         if !self.model.inline_mode {
             let _ = execute!(stdout, LeaveAlternateScreen);
         }
